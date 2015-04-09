476a8c0ecc1d98ef7bfe077760e0e85204bc4329
stanislav <natispu@hotmail.com>
2015-04-09 13:54:55 +0200

    added first Tests

---
+++ WhisperMessage/WhisperSecondNext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhisperMessage
+{
+    public class WhisperSecondNext: IWhisperString
+    {
+
+        public string ManipulateMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
