03a1db4e1a138349fd385dcddff8c635df1e9b80
Tobias Bladini <su@isometa.com>
2015-04-09 10:36:01 +0200

    Added two main tests for first modueule

---
+++ WhisperMessage/WhisperMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhisperMessage
+{
+    public class WhisperString : IWhisperString     
+    {
+        public string ManipulateMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
