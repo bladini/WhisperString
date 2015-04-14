5f4490859d53767b4d141fca3db106813a79291d
Noman Ellahi <ellahi_nomi@hotmail.com>
2015-04-14 21:14:45 +0200

    Add KokoWhisperString Test file

--- WhisperMessage/WhisperStringKoKo.cs
+++ WhisperMessage/WhisperStringKoKo.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WhisperMessage
 {
-    class WhisperStringKoKo
+    public class WhisperStringKoko : IWhisperString
     {
+        public string ManipulateMessage(string message)
+        {
+            if (message == null)
+                throw new ArgumentNullException("Message is null.");
+            if (message.Trim().Equals(""))
+                throw new ArgumentException("Message is empty.");
+
+            return Regex.Replace(message, "[c,k]", "koko");
+
+
+        }
     }
 }
