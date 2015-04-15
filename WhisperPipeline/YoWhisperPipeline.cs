d5a76e9c12d08d9d198cd47fd522306a1ae0f402
nikqi <timmi_nikqi@hotmail.com>
2015-04-15 15:51:43 +0200

    Removed all DLL files, and some exe

---
+++ WhisperPipeline/YoWhisperPipeline.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhisperMessage;
+using WhisperMessage.Pipeline;
+
+namespace WhisperPipeline
+{
+    public class YoWhisperPipeline : IWhisperPipeline
+    {
+        private IWhisperString _reverseWhisperMessage = new ReverseWhisperMessage();
+
+        private IWhisperString _whisperSecondNext = new WhisperSecondNext();
+
+        private IWhisperString _whisperStringKoko = new WhisperStringKoko();
+
+        public string Whisper(string message)
+        {
+            message = _reverseWhisperMessage.ManipulateMessage(message);
+
+            message = _whisperSecondNext.ManipulateMessage(message);
+
+            message = _whisperStringKoko.ManipulateMessage(message);
+
+            return message;
+        }
+    }
+
+
+}
