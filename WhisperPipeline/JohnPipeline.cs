d7f273c2418fad437fb762f9a71b449d960cb86d
John Fristedt <john@johnfristedt.com>
2015-04-19 20:07:10 +0200

    REFACTOR: Rhyme working

---
+++ WhisperPipeline/JohnPipeline.cs
@@ -0,0 +1,21 @@
+﻿using RhymeWhisperMessage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhisperMessage;
+using WhisperMessage.Pipeline;
+
+namespace WhisperPipeline
+{
+    class JohnPipeline : IWhisperPipeline
+    {
+        private IWhisperString rhyme = new RhymeWhisper();
+
+        public string Whisper(string message)
+        {
+            return rhyme.ManipulateMessage(message);
+        }
+    }
+}
