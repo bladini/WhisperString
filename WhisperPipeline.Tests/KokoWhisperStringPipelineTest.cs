eb8601a42e6af29c87851aff64515fa0d9a880a9
Noman Ellahi <ellahi_nomi@hotmail.com>
2015-04-20 14:06:08 +0200

    Rename the file for whisperSting Test and Whisper Pipeline

---
+++ WhisperPipeline.Tests/KokoWhisperStringPipelineTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WhisperPipeline.Tests
+{
+    [TestClass]
+    public class KokoWhisperStringPipelineTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
