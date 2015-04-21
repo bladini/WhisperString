354d01f948349d1a4d1f6666c9edd49f8a6cc856
Daniel79 <daniel@bitflippers.se>
2015-04-21 12:33:56 +0200

    Added code from Daniel, first time

---
+++ WhisperString.Test/CharToWordWhisperStringTests.cs
@@ -0,0 +1,26 @@
+﻿//Author Lisas module
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WhisperString.Test
+{
+    [TestClass]
+    public class CharToWordWhisperStringTest
+    {
+        IWhisperString _chartowordMessage = new CharToWordWhisperString();
+        [TestMethod]
+        public void CharToWordWhisperMessageTest()
+        {
+            //Arrange
+            string value = "a";
+            string expected = "apa";
+
+            //Act
+            string actual = _chartowordMessage.ManipulateMessage(value);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ WhisperString/CharToWordWhisperString.cs
@@ -0,0 +1,23 @@
+﻿//Author Daniels module
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhisperString
+{
+    public class CharToWordWhisperString : IWhisperString
+    {
+
+        public string ManipulateMessage(string message)
+        {
+
+            string result = message.Replace("a", "apa").Replace("b", "bebis").Replace("c", "cykel")
+            .Replace("d", "dremmel").Replace("i", "ii");
+
+            return result;
+        }
+    }
+}
