3d5fd32a2a99f9e66224c64f9b51bf8a5650d7c3
Enfeathered <gabriel.a.svensk@gmail.com>
2015-04-26 13:37:56 +0200

    Added pipeline and unit tests

---
+++ WhisperPipeline.Tests/CaesarCipherWhisperPipelineTests.cs
@@ -0,0 +1,21 @@
+﻿///Author: Gabriel
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhisperString.Pipeline;
+
+namespace WhisperPipeline.Tests {
+    [TestClass]
+    public class CaesarCipherWhisperPipelineTests {
+
+        [TestMethod]
+        public void CaesarCipherPipelineTest() {
+            var value = "abc";
+            var expected = "roroee";
+            var pipe = new CaesarCipherWhisperPipeline();
+
+            var actual = pipe.Whisper(value);
+            Assert.AreEqual(expected, actual);
+        }
+        
+    }
+}
