cd337f83abde55ab35b8df4d78fb20291ba1203f
Noman Ellahi <ellahi_nomi@hotmail.com>
2015-04-27 11:15:20 +0200

    Add Whisperpipeline and WhisperpipelineTest and rename the WhisperString File,Add auther name to these file.

---
+++ WhisperPipeline.Tests/KoKoWhisperPipelineTest.cs
@@ -0,0 +1,15 @@
+﻿// Auther : Noman Elahi
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WhisperPipeline.Tests
+{
+    [TestClass]
+    public class KoKoWhisperPipelineTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- WhisperPipeline/KokoWhisperPipeline.cs
+++ WhisperPipeline/KokoWhisperPipeline.cs
@@ -3,30 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WhisperString;
-using WhisperString.Pipeline;
 
 namespace WhisperPipeline
 {
-    class NomanPipeline
+    class KoKoWhisperPipeline
     {
-        public class NomanPipeline : IWhisperPipeline
-        {
-            private IWhisperString whisperYo = new YoWhisperString();
-            private IWhisperString whisperReverse = new ReverseWhisperMessage();
-            private IWhisperString whisperSecondNext = new WhisperSecondNext();
-
-
-            public string Whisper(string message)
-            {
-                message = whisperYo.ManipulateMessage(message);
-                message = whisperReverse.ManipulateMessage(message);
-                message = whisperSecondNext.ManipulateMessage(message);
-
-                return message;
-
-            }
-
-        }
     }
 }
